49692442991287a3ae19a0910a71834e166b9e66
StarLikeRain <pengyaokang@qq.com>
2020-06-27 18:48:43 +0800

    init project

---
+++ build/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "main.js"
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  devtool: process.env.NODE_ENV === 'production' ? false : 'inline-source-map'
+}
