c11e72f309a3041c155ead7876a9448c93fb8d83
StarLikeRain <pengyaokang@qq.com>
2020-07-12 15:48:03 +0800

    上次用typeof，这次用instanceOf可以判断class

--- src/example/advanced-type.ts
+++ src/example/advanced-type.ts
@@ -8,12 +8,30 @@ const getRandomValue = () => {
 }
 // 对返回值类型存在随机性
 const item = getRandomValue()
-// 类型保护
+// 类型保护可以用 typeof 也能用 instanceOf
 function isString(value: number | string): value is string {
+    // 类型保护只能是下面4种中的一种 string/number/boolean/symbol
     return typeof value === 'string'
 }
 if (isString(item)) {
     console.log(item.length)
 } else {
     console.log(item.toFixed())
+}
+
+class A {
+    constructor(public name: string) { }
+}
+class B {
+    constructor(public age: number) { }
+}
+function randomClass() {
+    return Math.random() > 0.5 ? new A('卧槽') : new B(18)
+}
+let instance = randomClass()
+if (instance instanceof A) {
+    console.log(instance.name)
+} else {
+    console.log(instance.age)
+
 }
